f0274ca4fb79a7b086c351d814898d21d23bb62c
Cezar Augusto <boss@cezaraugusto.net>
2022-02-13 23:16:15 -0300

    Add extension-create samples

---
+++ extension-create/demo-extension/devtools/sidebar.js
@@ -0,0 +1,11 @@
+/* global chrome */
+
+chrome
+  .devtools
+  .panels
+  .elements
+  .createSidebarPane('My Own Sidebar', sidebar => {
+    browser.devtools.panels.elements.onSelectionChanged.addListener(() => {
+      sidebar.setExpression('$0')
+    })
+  })
